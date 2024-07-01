fa654a9727b3148bbaa6e5b0bd003f87d270b319
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-07-01 14:06:56 +0700

    add admin authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        // TODO If admin, redirect to admin dashboard
+        if($request->user()->usertype === 'admin'){
+            return redirect('admin/dashboard');
+        }
+
+        return redirect()->intended(route('dashboard'));
     }
 
     /**
